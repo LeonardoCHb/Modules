a60f08aa1aa174cfcf393774c94225af5fcb5922
LeonardoCHb <leonardohably@hotmail.com>
2021-01-07 18:31:54 -0300

    routes and some folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import Test from '~/components/Test'
+import React from 'react'
+import Routes from './routes'
 
-function App() {
-  return (
-    <div className="App">
-      <Test/>
-    </div>
-  );
-}
+const App = () => <Routes/>
 
 export default App;
