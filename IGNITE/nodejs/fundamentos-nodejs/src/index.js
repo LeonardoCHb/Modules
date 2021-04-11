99475a87ffc10426ce0bcd41956e0c3f6e09ec6b
LeonardoCHb <leonardohably@hotmail.com>
2021-04-11 08:01:53 -0300

    Configurando projeto => aula 02

--- IGNITE/nodejs/fundamentos-nodejs/src/index.js
+++ IGNITE/nodejs/fundamentos-nodejs/src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (request,response) => {
-    return response.json({message: "FALA TU"})
+    return response.json({message: "FALA TU, SEU SAFADO"})
 })
 
 app.listen(3333)
