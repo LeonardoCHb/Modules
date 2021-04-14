ecf3715eb91f573975a917f16556b1fa23d8ddf1
LeonardoCHb <leonardohably@hotmail.com>
2021-04-14 10:44:56 -0300

    Primeiro projeto com NodeJS => Aula 04

--- IGNITE/nodejs/fundamentos-nodejs/src/index.js
+++ IGNITE/nodejs/fundamentos-nodejs/src/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
 
@@ -27,6 +28,14 @@ app.post("/account", (request,response) => {
 
     return response.status(201).send()
     
+})
+
+app.get("/statement/:cpf", (request,response) => {
+    const { cpf } = request.params
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    return response.json(customer.statemnt)
 })
 
 
